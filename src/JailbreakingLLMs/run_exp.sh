175a4977577ecedf575e075abf7d56d7ecc81283
William Agnew <wagnew@andrew.cmu.edu>
2024-07-26 21:34:05 -0400

    slurm array run

--- src/JailbreakingLLMs/run_exp.sh
+++ src/JailbreakingLLMs/run_exp.sh
@@ -8,10 +8,11 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=2:00:00
-#SBATCH --gres=gpu:A6000:1 
+#SBATCH --gres=gpu:A6000:1
+#SBATCH --mem=48G 
 
 # Your job commands go here
 cd /home/wagnew/github/LLMDataDefenses/src/JailbreakingLLMs
 conda activate resist_tor_311
 
-python run_exp.py --experiment_num=$SLURM_ARRAY_TASK_ID
+python main.py --experiment_num=$SLURM_ARRAY_TASK_ID
